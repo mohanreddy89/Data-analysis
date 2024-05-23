b4b0fe5c8c1e15c9741b7284c67ba527603e703c
mohanreddy89 <112534518+mohanreddy89@users.noreply.github.com>
2024-05-23 09:43:18 +0530

    Data Analysis.sql

---
+++ Data Analysis.sql
@@ -0,0 +1,43 @@
+-- select * from demo.df_orders;
+-- Find top 10 highest revenue generating products 
+select product_id,sum(sale_price) as sales 
+from demo.df_orders 
+group by product_id 
+order by sales desc limit 10;
+
+-- Find top 5 highest selling products in each region
+with cte as(
+select region,product_id,sum(sale_price) as sales
+from demo.df_orders 
+group by region,product_id)
+select * from(
+select *
+, row_number() over(partition by region order by sales desc) as rn from cte) A 
+where rn<=5
+
+-- find month over month growth comparison for 2022 and 2023 sales eg : jan 2022 vs jan 2023
+with cte as(
+select distinct year(order_date) as order_year,month(order_date) as order_month,
+sum(sale_price)  as sales from demo.df_orders
+group by year(order_date),month(order_date)
+)
+select order_month 
+, sum(case when order_year=2022 then sales else 0 end) as sales_2022
+, sum(case when order_year=2022 then sales else 0 end) as sales_2023
+from cte
+group by order_month
+order by order_month
+
+-- for each category which month had highest sales 
+with cte as (
+select category , format(order_date,'yyyyMM') as order_year_month 
+,sum(sale_price) as sales 
+from demo.df_orders
+group by category,format(order_date,'yyyyMM')
+)
+select * from(
+select *,
+row_number() over (partition by category order by sales desc) as rn
+from cte 
+) a
+where rn =1
